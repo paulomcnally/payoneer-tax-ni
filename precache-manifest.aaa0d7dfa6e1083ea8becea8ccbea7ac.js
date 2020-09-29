622833850ba43a84133a3f51bc36957c7f5c2efa
Paulo McNally <paulomcnally@gmail.com>
2020-09-29 11:40:58 -0600

    Updates

---
+++ precache-manifest.aaa0d7dfa6e1083ea8becea8ccbea7ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c62af87ddb77a27d0860093e7b9b083d",
+    "url": "/payoneer-tax-ni/index.html"
+  },
+  {
+    "revision": "48f5b8322b6024f17254",
+    "url": "/payoneer-tax-ni/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "184d78de1e302a517208",
+    "url": "/payoneer-tax-ni/static/js/2.a3f0a4c2.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/payoneer-tax-ni/static/js/2.a3f0a4c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48f5b8322b6024f17254",
+    "url": "/payoneer-tax-ni/static/js/main.c5b7b794.chunk.js"
+  },
+  {
+    "revision": "d8b88c6c54bb14dbf162",
+    "url": "/payoneer-tax-ni/static/js/runtime-main.eada6c69.js"
+  }
+]);
